f6eb8773ad6d79588f8b30ebb992b70fd25b044a
Gabriela Chiquetto <gabrielachiquetto@gmail.com>
2021-08-16 12:21:34 -0300

    Adding readme image

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from tkinter import *
 import socket
 
-
 tela_inicial = Tk()
 tela_inicial.title('LOCALIZADOR DE IP')
 tela_inicial.geometry("300x150+800+450")
 tela_inicial.resizable(False, False)
 tela_inicial.iconbitmap("image/icone.ico")
 resultado = StringVar()
-
 def buscar():
     nome = socket.gethostname()
     ip = socket.gethostbyname(nome)
@@ -19,10 +17,7 @@ lbl_espaco = Label(tela_inicial, width = 45, ).grid(row=0, columnspan=2)
 lbl_ip = Label(tela_inicial, text= 'IPV4: ', width=20, ).grid(columnspan=2, sticky='WE')
 lbl_resultado = Label(tela_inicial, textvariable=resultado, width=15, height=3, fg='red').grid(columnspan=2,row=2)
 
-
 #button
 btn = Button(tela_inicial, text="Pesquisar", command=buscar).grid(columnspan=2,row=3)
-
-
 
 tela_inicial.mainloop()
